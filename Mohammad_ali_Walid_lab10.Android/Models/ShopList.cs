0e5106b8aa819f1f7a823d5213e0971968ae01be
Walid <Mohammad.Kh.Walid@utcluj.didatec.ro>
2021-12-12 18:06:51 +0200

    Add project files.

---
+++ Mohammad_ali_Walid_lab10.Android/Models/ShopList.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mohammad_ali_Walid_lab10.Droid.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Mohammad_ali_Walid_lab10/Models/ShopList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace Mohammad_ali_Walid_lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
